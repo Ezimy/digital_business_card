1504b0799f5bdfaac2f6b90611f20d0e13518f02
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-15 15:46:59 -0800

    fixed line height and added button functionallity

--- src/components/Butttons.jsx
+++ src/components/Butttons.jsx
@@ -3,14 +3,14 @@ import React from "react"
 export default function ProfilePicture(){
     return(
         <div className="button-section">
-            <div id="email-btn" className="button-element">
+            <a href="mailto:yang.sammy005@gmail.com" id="email-btn" className="button-element">
                 <img src="/src/images/Mail.svg"/>
                 <p>Email</p>
-            </div>
-            <div id="linkedin-btn" className="button-element">
+            </a>
+            <a href="https://www.linkedin.com/in/sammy-yang29/" target="_blank" id="linkedin-btn" className="button-element">
                 <img src="/src/images/linkedin.svg"/>
                 <p>LinkedIn</p>
-            </div>
+            </a>
         </div>
     )
 }
