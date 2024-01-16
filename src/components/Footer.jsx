80c90ffb2da48ed3e4711bf6437239d54f4d29f7
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2024-01-15 17:07:13 -0800

    added anchor tags to icons and changed css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,16 @@ import React from "react"
 export default function Footer(){
     return(
         <div className="footer">
-            <img src="/src/images/Twitter-Icon.svg" alt="twitter-icon"/>
+            <a href="https://twitter.com/Sammy0669" target="_blank">
+                <img src="/src/images/Twitter-Icon.svg" alt="twitter-icon"/>
+            </a>
             <img src="/src/images/Facebook-Icon.svg" alt="facebook-icon"/>
-            <img src="/src/images/Instagram-Icon.svg" alt="instagram-icon"/>
-            <img src="/src/images/GitHub-Icon.svg" alt="github-icon"/>
+            <a href="https://www.instagram.com/yang.sammy005/">
+                <img src="/src/images/Instagram-Icon.svg" alt="instagram-icon"/>
+            </a>
+            <a href="https://github.com/Ezimy" target="_blank">
+                <img src="/src/images/GitHub-Icon.svg" alt="github-icon"/>
+            </a>
         </div>
     )
 }
